e86c5ce7f3900d17f57665a545190ec675dd5563
12Shamali12 <114297778+12shamali12@users.noreply.github.com>
2025-10-06 16:04:59 +0300

    finally finished my first game it's just for learning unity

--- Assets/scripts/Driver.cs
+++ Assets/scripts/Driver.cs
@@ -4,20 +4,30 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Rendering.RenderGraphModule;
 using UnityEngine.UIElements;
+using TMPro;
+using Unity.VisualScripting;
+
 
 public class Driver : MonoBehaviour
-{   bool has_package = false;
-  float destroy_delay = 0.08f;
+{
+    bool has_package = false;
+    float destroy_delay = 0.08f;
+    [SerializeField] TMP_Text boost_text;
+    [SerializeField] TMP_Text delivery_text, del_text;
 
-    float move_speed = 10f;
+    float move_speed = 9.5f;
     float rotate_speed = 170f;
-    float boost_speed = 15f;
+    float boost_speed = 13f;
     float normal_speed = 10f;
-   public ParticleSystem cd;
-     bool is_boosted = false;
+    public ParticleSystem cd;
+    bool is_boosted = false;
     void Start()
-    {    GetComponent<ParticleSystem>().Stop();
+    {
+        boost_text.gameObject.SetActive(false);
+        GetComponent<ParticleSystem>().Stop();
         cd = GetComponent<ParticleSystem>();
+        del_text.gameObject.SetActive(false);
+                delivery_text.gameObject.SetActive(false);
     }
 
     void OnTriggerEnter2D(Collider2D collision)
@@ -25,10 +35,10 @@ public class Driver : MonoBehaviour
         ParticleSystem.MainModule ps = cd.main;
 
         if (collision.CompareTag("boost") && !is_boosted)
-        {
+        {   boost_text.gameObject.SetActive(true);
             is_boosted = true;
             move_speed = boost_speed;
-            rotate_speed = 215;
+            rotate_speed = 200;
             Destroy(collision.gameObject, 0.08f);
             if (!has_package)
             {
@@ -46,7 +56,8 @@ public class Driver : MonoBehaviour
             if (has_package)
                 Debug.Log("already have package");
             else
-            {
+            {   del_text.gameObject.SetActive(true);
+                delivery_text.gameObject.SetActive(true);
                 has_package = true;
                 if (is_boosted)
                     ps.startColor = Color.burlywood;
@@ -57,15 +68,17 @@ public class Driver : MonoBehaviour
                 Destroy(collision.gameObject, destroy_delay);
             }
         }
-       
+
         if (collision.CompareTag("customer"))
         {
             if (has_package)
-            {
+            {   del_text.gameObject.SetActive(false);
+                delivery_text.gameObject.SetActive(false);
                 has_package = false;
                 if (is_boosted) { ps.startColor = Color.grey; }
                 else { cd.Stop(); }
                 Debug.Log("delivered");
+                Destroy(collision.gameObject, destroy_delay);
             }
             else
             {
@@ -75,16 +88,18 @@ public class Driver : MonoBehaviour
     }
 
     void OnCollisionEnter2D(Collision2D collision)
-    { ParticleSystem.MainModule ps = cd.main;
+    {   boost_text.gameObject.SetActive(false);
+        ParticleSystem.MainModule ps = cd.main;
         is_boosted = false;
         move_speed = normal_speed;
         rotate_speed = 170;
         if (has_package) { ps.startColor = Color.lightPink; }
         else
-        cd.Stop();
+            cd.Stop();
     }
 
-  void Update(){
+    void Update()
+    {
 
         float move = 0f;
         float steer = 0f;
@@ -95,27 +110,27 @@ public class Driver : MonoBehaviour
             /* if (Keyboard.current.shiftKey.isPressed)
                  move = 1.5f;
              else*/
-            move = 1f;  
+            move = 1f;
         }
         else if (Keyboard.current.sKey.isPressed || Keyboard.current.downArrowKey.isPressed)
         {
             /*if (Keyboard.current.shiftKey.isPressed)
                 move = -1.25f;
             else*/
-                move = -1f;
+            move = -1f;
         }
-       if (Keyboard.current.dKey.isPressed || Keyboard.current.rightArrowKey.isPressed)
-       {
-           /*if (Keyboard.current.shiftKey.isPressed)
-               steer = -1.25f;
-           else*/
-               steer = -1f;
-       }
-       else if (Keyboard.current.aKey.isPressed || Keyboard.current.leftArrowKey.isPressed)
+        if (Keyboard.current.dKey.isPressed || Keyboard.current.rightArrowKey.isPressed)
+        {
+            /*if (Keyboard.current.shiftKey.isPressed)
+                steer = -1.25f;
+            else*/
+            steer = -1f;
+        }
+        else if (Keyboard.current.aKey.isPressed || Keyboard.current.leftArrowKey.isPressed)
         {/*if (Keyboard.current.shiftKey.isPressed)
                 steer = 1.25f;
                 else*/
-                steer = 1f;
+            steer = 1f;
         }
 
         float steer_amount = steer * rotate_speed * Time.deltaTime;
